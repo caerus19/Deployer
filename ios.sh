322c983625eb73a7125f33fac21f788b68c218f4
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-22 17:37:26 +0400

    Create ios.sh

---
+++ ios.sh
@@ -0,0 +1,27 @@
+MESAJ="DTÖUserBot Avtomatik Deploy Qurulumuna Xoş Geldiniz"
+MESAJ+="\nTelegram: @DTOUserBot"
+echo y | apk update
+clear
+echo -e $MESAJ
+echo "Python yüklenir"
+echo y | apk add python3
+clear
+echo -e $MESAJ
+echo "Git yüklenir"
+echo y | apk add git
+clear
+echo -e $MESAJ
+echo "TeleThon yüklenir"
+python3 -m pip install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/umudmmmdov1/Qurulum
+clear
+echo -e $MESAJ
+clear
+echo -e $MESAJ
+echo "Kitabxana yüklenir"
+cd Qurulum
+python3 -m pip install wheel
+python3 -m pip install -r requirements.txt
+clear
+python3 -m dto_installer
