654af6a2087e75da371cee12fc75d8a3831e73d7
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-22 17:40:00 +0400

    Create windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,14 @@
+Write-Output("@DTOUserBot Windows Auto-Deploy")
+Write-Output("Scoop yüklenir...")
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Write-Output("Python & Git yüklenir...")
+scoop install git python@3.8
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Output("Qurulum yüklenir...")
+
+git clone https://github.com/umudmmmdov1/Qurulum
+Set-Location Qurulum
+python3.8 -m pip install -r requirements.txt
+python3.8 -m dto_installer
