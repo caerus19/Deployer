0e72abbbf5b3c3bf1f3d0275630b0b59a897a465
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-12-02 03:04:44 +0400

    Create alternativ.sh

---
+++ alternativ.sh
@@ -0,0 +1,26 @@
+MESAJ="DTÖUserBot Avtomatik Deploy Qurulumuna Xoş Geldiniz"
+MESAJ+="\nTelegram: @DTOUserBot"
+apt update 
+clear
+echo -e $MESAJ
+echo "Python3 yüklenir"
+apt install python3 
+clear
+echo -e $MESAJ
+echo "Git yüklenir"
+apt install git
+clear
+echo -e $MESAJ
+echo "TeleThon yüklenir"
+pip3 install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/umudmmmdov1/Qurulum
+clear
+echo -e $MESAJ
+cd Qurulum
+clear
+echo "Kitabxana yüklenir"
+echo -e $MESAJ
+pip3 install wheel
+pip3 install -r requirements.txt
+python3 -m dto_installer
